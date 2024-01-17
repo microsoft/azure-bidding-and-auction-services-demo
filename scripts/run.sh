4ac4d475d38dca5904a5359d5f53e2f5153a82d5
Dominic Ayre <dominicayre@microsoft.com>
2024-01-17 16:17:27 +0000

    Improve B&A cleanup

--- scripts/run.sh
+++ scripts/run.sh
@@ -3,6 +3,8 @@ if [ -z "$DEMO_WORKSPACE" ]; then
     DEMO_WORKSPACE=~/demo
 fi
 
+REPO_PATH=$(realpath $(dirname "$0")/..)
+
 # Start KMS inside a CCF container
 echo "Running KMS inside docker container..."
 CONTAINER_ID=$(docker run \
@@ -13,25 +15,37 @@ CONTAINER_ID=$(docker run \
     kms \
     /bin/bash -c ". /root/.nvm/nvm.sh && npm install && make start-host")
 
-# Once the KMS is running, add a key
+# Wait for the KMS to start
 echo "Container started"
 echo "Waiting for KMS server to start..."
+LAST_LINE=""
 while ! docker exec $CONTAINER_ID /bin/bash -c "lsof -i :8000" > /dev/null 2>&1; do
-    sleep 1
+    CUR_LINE=$(docker logs "$CONTAINER_ID" --tail 1 | cut -c 1-80)
+    if [ "$CUR_LINE" != "$LAST_LINE" ]; then
+        echo -en "\r\033[K>  $CUR_LINE..."
+        LAST_LINE=$CUR_LINE
+    fi
 done
-echo "KMS server started"
+echo -e "\nKMS server started"
+
+# Add a key to the KMS
 echo "Adding Key to KMS..."
 docker exec $CONTAINER_ID /bin/bash -c "make setup" > /dev/null 2>&1
 echo "Key added"
 
 # Run the B&A servers
+declare -A service_pid
 (
     cd $DEMO_WORKSPACE/bidding-auction-servers
-    source ./env
+    source $REPO_PATH/scripts/env
     ./bazel-bin/services/bidding_service/server --init_config_client="true" &
+    service_pid["bidding"]=$!
     ./bazel-bin/services/buyer_frontend_service/server --init_config_client="true" &
+    service_pid["bfe"]=$!
     ./bazel-bin/services/auction_service/server --init_config_client="true" &
+    service_pid["auction"]=$!
     ./bazel-bin/services/seller_frontend_service/server --init_config_client="true" &
+    service_pid["sfe"]=$!
 )
 
 # Wait until the user presses Ctrl+C
@@ -40,7 +54,9 @@ cleanup() {
     docker rm -f $CONTAINER_ID > /dev/null 2>&1
 
     # Stop the B&A servers
-    pkill -f bazel-bin
+    for service in "${!service_pid[@]}"; do
+        kill ${service_pid[$service]} 2> /dev/null
+    done
 
     exit 0
 }
